cd9d43c84d6e4448f4a0a29bf86f727ef1b6ded0
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-05-17 08:13:10 -0300

    pagina home

--- resources/views/App/home.blade.php
+++ resources/views/App/home.blade.php
@@ -5,6 +5,27 @@
 
 @section('content')
 
-@endsection
 
-@include('Layouts.footer')
+
+    <div class="hero">
+
+        <div class="hero-intro">
+            <h1>
+                Conectamos quem cuida a quem precisa. <strong>Simples, rápido e humano.</strong>
+            </h1>
+            <p>
+                Encontre o cuidador ideal para suas necessidades ou ofereça seus serviços de cuidado de forma fácil
+                e segura. Nossa plataforma simplifica a busca e a conexão, priorizando o cuidado humanizado e a
+                confiança entre as partes.
+            </p>
+            <a href="#"class="btn btn-success">Quero saber mais</a>
+        </div>
+        <div class="hero-img">
+            <img src="{{ asset('assets/imgs/hero4.png') }}" alt="Imagem de fundo">
+        </div>
+    </div>
+    </section>
+
+    @include('Layouts.footer')
+
+@endsection
--- resources/views/Layouts/main_layout.blade.php
+++ resources/views/Layouts/main_layout.blade.php
@@ -30,10 +30,14 @@
 
 <body>
 
-    @yield('content')
+    <main>
+        @yield('content')
+    </main>
 
     {{-- link js bootstrap --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
