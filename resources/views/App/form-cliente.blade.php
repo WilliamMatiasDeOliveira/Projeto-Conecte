b7f8b9f777c488fc060cdcd9fb1fb4faa9895e61
Vinicius Leonardo Silveira <vinicius.silveira9@fatec.sp.gov.br>
2025-05-26 11:12:54 -0300

    css cadastro+classes no html

--- resources/views/App/form-cliente.blade.php
+++ resources/views/App/form-cliente.blade.php
@@ -12,94 +12,94 @@
 
                 <input type="hidden"name="tipo"value="cliente">
 
-                <div>
-                    <label for="nome">Nome</label>
-                    <input type="text"name="nome" class="form-control"value="{{ old('nome') }}">
-                    @error('nome')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
+                <div class="form-cliente-pessoal">
+                    <div>
+                        <label for="nome">Nome</label>
+                        <input type="text"name="nome" class="form-control"value="{{ old('nome') }}">
+                        @error('nome')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="cpf">Cpf</label>
+                        <input type="text"name="cpf" class="form-control"value="{{ old('cpf') }}">
+                        @error('cpf')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="email">E-mail</label>
+                        <input type="email"name="email" class="form-control"value="{{ old('email') }}">
+                        @error('email')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="telefone">Telefone</label>
+                        <input type="text"name="telefone" class="form-control"value="{{ old('telefone') }}">
+                        @error('telefone')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
                 </div>
 
-                <div>
-                    <label for="email">E-mail</label>
-                    <input type="email"name="email" class="form-control"value="{{ old('email') }}">
-                    @error('email')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
+                <div class="form-cliente-endereco">
+                    <div>
+                        <label for="cidade">Cidade</label>
+                        <input type="text"name="cidade" class="form-control"value="{{ old('cidade') }}">
+                        @error('cidade')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="bairro">Bairro</label>
+                        <input type="text"name="bairro" class="form-control"value="{{ old('bairro') }}">
+                        @error('bairro')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="rua">Rua</label>
+                        <input type="text"name="rua" class="form-control"value="{{ old('rua') }}">
+                        @error('rua')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
                 </div>
 
-                <div>
-                    <label for="telefone">Telefone</label>
-                    <input type="text"name="telefone" class="form-control"value="{{ old('telefone') }}">
-                    @error('telefone')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <label for="cpf">Cpf</label>
-                    <input type="text"name="cpf" class="form-control"value="{{ old('cpf') }}">
-                    @error('cpf')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <label for="cidade">Cidade</label>
-                    <input type="text"name="cidade" class="form-control"value="{{ old('cidade') }}">
-                    @error('cidade')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <label for="bairro">Bairro</label>
-                    <input type="text"name="bairro" class="form-control"value="{{ old('bairro') }}">
-                    @error('bairro')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <label for="rua">Rua</label>
-                    <input type="text"name="rua" class="form-control"value="{{ old('rua') }}">
-                    @error('rua')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <label for="password">Senha</label>
-                    <input type="password"name="password" class="form-control">
-                    @error('password')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <label for="password_confirmation">Confirmar Senha</label>
-                    <input type="password"name="password_confirmation" class="form-control">
-                    @error('password_confirmation')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
+                <div class="form-cliente-senha">
+                    <div>
+                        <label for="password">Senha</label>
+                        <input type="password"name="password" class="form-control">
+                        @error('password')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="password_confirmation">Confirmar Senha</label>
+                        <input type="password"name="password_confirmation" class="form-control">
+                        @error('password_confirmation')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
                 </div>
 
                 <div>
