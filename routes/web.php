e918bfd94251cfd6ed5b2f11ff4ca3cc9d75a50a
Willam Matias de Oliveira <william.oliveira@fatec.sp.gov.br>
2025-05-27 12:05:58 -0300

    route group by controller

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,47 @@ use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AppController::class, 'home'])->name('home');
-Route::get('/sobre', [AppController::class, 'sobre'])->name('sobre');
-Route::get('/cadastro', [AppController::class, 'cadastro'])->name('cadastro');
-Route::get('/contatos', [AppController::class, 'contatos'])->name('contatos');
-Route::get('/form-cliente', [AppController::class, 'form_cliente'])->name('form.cliente');
-Route::get('/form-cuidador', [AppController::class, 'form_cuidador'])->name('form.cuidador');
-Route::get('/login', [AppController::class, 'login'])->name('login');
+Route::controller(AppController::class)->group(function() {
 
-Route::post('/form-cuidador-submit', [AuthController::class, 'form_cuidador_submit'])->name('form.cuidador.submit');
-Route::post('/form-cliente-submit', [AuthController::class, 'form_cliente_submit'])->name('form.cliente.submit');
-Route::post('/login-submit', [AuthController::class, 'login_submit'])->name('login.submit');
+    Route::get('/', 'home')->name('home')
+        ->middleware(['guest', 'auth']);
 
-Route::get('/dashboard-cliente', [AppController::class, 'dashboard_cliente'])->name('dashboard.cliente');
-Route::get('/dashboard-cuidador', [AppController::class, 'dashboard_cuidador'])->name('dashboard.cuidador');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/sobre', 'sobre')->name('sobre')
+        ->middleware(['guest', 'auth']);
+
+    Route::get('/cadastro', 'cadastro')->name('cadastro')
+        ->middleware(['guest']);
+
+    Route::get('/contatos', 'contatos')->name('contatos')
+        ->middleware(['guest', 'auth']);
+
+    Route::get('/form-cliente', 'form_cliente')->name('form.cliente')
+        ->middleware(['guest']);
+
+    Route::get('/form-cuidador', 'form_cuidador')->name('form.cuidador')
+        ->middleware(['guest']);
+
+    Route::get('/login', 'login')->name('login')
+        ->middleware(['guest']);
+
+    Route::get('/dashboard-cliente', 'dashboard_cliente')->name('dashboard.cliente')
+        ->middleware(['auth']);
+
+    Route::get('/dashboard-cuidador', 'dashboard_cuidador')->name('dashboard.cuidador')
+        ->middleware(['auth']);
+});
+
+Route::controller(AuthController::class)->group(function() {
+
+    Route::post('/form-cuidador-submit', 'form_cuidador_submit')->name('form.cuidador.submit')
+        ->middleware(['guest']);
+
+    Route::post('/form-cliente-submit', 'form_cliente_submit')->name('form.cliente.submit')
+        ->middleware(['guest']);
+
+    Route::post('/login-submit', 'login_submit')->name('login.submit')
+        ->middleware(['guest']);
+    
+    Route::post('/logout', 'logout')->name('logout')
+        ->middleware(['auth']);
+});
