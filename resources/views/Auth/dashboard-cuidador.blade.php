57e0d4bf3e1e6debbedd2dcab5e18693a5a64861
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-06-04 08:27:33 -0300

    ajustando dashboard-cuidador

--- resources/views/Auth/dashboard-cuidador.blade.php
+++ resources/views/Auth/dashboard-cuidador.blade.php
@@ -1,30 +1,34 @@
 @extends('Layouts.main_layout')
 @section('title' . 'dashboard-cuidador')
 
-
-
 @section('content')
 
     <section class="dashboard-cuidador-container">
 
         <div class="dashboard-cuidador">
-            <div class="dashboard-cuidador img-user">
-                <img src="{{ asset('assets/imgs/cuidadores/' . Auth::user()->foto) }}" alt="foto do usuario">
-                <h1 class='dashboard-cuidador title-nome'>{{ Auth::user()->nome }}</h1>
+
+            <div class="dasboard-cuidador box-dados">
+                <div class="img-user">
+                    <img src="{{ asset('assets/imgs/cuidadores/' . Auth::user()->foto) }}" alt="foto do usuario">
+                    <h1 class='dashboard-cuidador title-nome'>{{ Auth::user()->nome }}</h1>
+                </div>
+
+                <div class="dashboard-cuidador dados">
+                    <p>E-MAIL : {{ Auth::user()->email }}</p><br>
+                    <p>TELEFONE : {{ Auth::user()->telefone }}</p><br>
+                    <p>CPF : {{ Auth::user()->cpf }}</p><br>
+                    <p>CIDADE : {{ Auth::user()->cidade }}</p><br>
+                    <p>BAIRRO : {{ Auth::user()->bairro }}</p><br>
+                    <p>RUA : {{ Auth::user()->rua }}</p><br>
+                </div>
             </div>
 
-            <div class="dashboard-cuidador info-user">
-                <p>E-MAIL : {{ Auth::user()->email }}</p>
-                <p>TELEFONE : {{ Auth::user()->telefone }}</p>
-                <p>CPF : {{ Auth::user()->cpf }}</p>
-                <p>CIDADE : {{ Auth::user()->cidade }}</p>
-                <p>BAIRRO : {{ Auth::user()->bairro }}</p>
-                <p>RUA : {{ Auth::user()->rua }}</p>
-                <p><iframe src="{{ asset('assets/imgs/curriculos/' . Auth::user()->curriculo) }}" width="100%"
-                        height="600px"></iframe></p>
-            </div>
+            <div class="dashboard-cuidador show-data">
+                <p><iframe src="{{ asset('assets/imgs/curriculos/' . Auth::user()->curriculo) }}"></iframe></p>
 
-            <a href="{{route('update', encrypt(Auth::user()->id))}}" class="btn btn-danger mt-4 mb-4">Atualizar Dados</a>
+                <a href="{{ route('update', encrypt(Auth::user()->id)) }}" class="btn btn-danger">Atualizar
+                    Dados</a>
+            </div>
 
 
         </div>
